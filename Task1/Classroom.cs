d9e7dfb0d601f06e4097b280fc81af858c69f743
Marat <milya010790@mail.ru>
2024-02-13 14:25:14 +0500

    Добавьте файлы проекта.

---
+++ Task1/Classroom.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1
+{
+    /// <summary>Класс</summary>
+    public class Classroom
+    {
+        /// <summary>Список учеников</summary>
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+          {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir","Evgeniy"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        /// <summary> </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            if (classes == null)
+                throw new ArgumentNullException("Список не может быть пустым");
+
+
+            //var uinted1 = classes.SelectMany(a => a.Students).ToArray();           
+
+            var united2 = (from a in classes
+                          from b in a.Students
+                          select b).ToArray();
+
+            return united2;
+
+        }
+    }
+}
